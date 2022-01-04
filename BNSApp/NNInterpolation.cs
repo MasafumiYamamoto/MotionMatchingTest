a53f370078835e9afa37c3239951f6d8a8d76675
masafumi-yamamoto <mythmaker3633@yahoo.co.jp>
2022-01-04 11:34:42 +0900

    最近傍探索実装

--- BNSApp/NNInterpolation.cs
+++ BNSApp/NNInterpolation.cs
@@ -2,9 +2,13 @@
 
 namespace BNSApp
 {
-    public class NNInterpolation
+    /// <summary>
+    /// 最近傍ポーズを利用して次のポーズを計算する
+    /// 前後方向から計算して次のポーズをブレンドしながら求めている
+    /// </summary>
+    public class NNInterpolation : ISolver
     {
-        public static void Solve(MotionData motionData, MotionContainer trainData){
+        public void Solve(MotionData motionData, MotionContainer trainData){
             for (int loop = 0; loop < motionData.Loop; loop++)
             {
                 var prevMeasuredId = loop * motionData.SkipFrames + 1;
@@ -33,76 +37,18 @@ namespace BNSApp
 
                 for (int i = 1; i < motionData.SkipFrames; i++)
                 {
-                    var pos = Merge(forwardList[i - 1], backwardList[^i], (float)(i - 1) / (motionData.SkipFrames - 2));
+                    var pos = Merge.LinearBlend(forwardList[i - 1], backwardList[^i], (float)(i - 1) / (motionData.SkipFrames - 2));
                     motionData.PosList[prevMeasuredId - 1 + i] = pos;
                 }
             }
         }
 
-        private static Pose Merge(Pose forward, Pose backward, float ratio)
-        {
-            var p0 = forward.Mult(1 - ratio);
-            var p1 = backward.Mult(ratio);
-            return p0.Add(p1);
-        }
-
         static Pose Interp(MotionContainer trainData,Pose pos, int direction, int motionId)
         {
-            FindNearestMotion(trainData, pos, direction, motionId, out var nearestCurrentPose, out var nearestFuturePose);
-            var delta = ComputeDelta(nearestCurrentPose, nearestFuturePose);
+            Search.FindNearestMotion(trainData, pos, direction, motionId, out var nearestCurrentPose, out var nearestFuturePose);
+            var delta = Utils.ComputeDelta(nearestCurrentPose, nearestFuturePose);
             var newPos = pos.Add(delta);
             return newPos;
-        }
-
-        static Pose ComputeDelta(Pose currentPose, Pose futurePose)
-        {
-            return futurePose.Minus(currentPose);
-        }
-
-        static void FindNearestMotion(MotionContainer trainData, Pose pose, int direction, int motionId, out Pose nearestCurrentPose, out Pose nearestFuturePose)
-        {
-            var minCost = float.MaxValue;
-            nearestCurrentPose = new Pose();
-            nearestFuturePose = new Pose();
-            foreach (var motion in trainData.MotionList)
-            {
-                for (int i = 0; i < motion.Length; i++)
-                {
-                    if (i - direction < 0 || motion.Length <= i - direction)
-                    {
-                        continue;
-                    }
-
-                    if (i + direction < 0 || motion.Length <= i + direction)
-                    {
-                        continue;
-                    }
-
-                    var currentPose = motion.PosList[i];
-                    var cost = CalcRootCost(currentPose, pose);
-                    if (cost < minCost)
-                    {
-                        minCost = cost;
-                        nearestCurrentPose = currentPose;
-                        nearestFuturePose = motion.PosList[i + direction];
-                    }
-                }
-            }
-        }
-
-        static float CalcRootCost(Pose p0, Pose p1)
-        {
-            var cost = 0f;
-            var root0 = p0.Joints[0];
-            var root1 = p1.Joints[0];
-            for (int i = 0; i < p0.Joints.Count; i++)
-            {
-                var j0 = p0.Joints[i] -root0;
-                var j1 = p1.Joints[i] - root1;
-                cost += (j0 - j1).Length();
-            }
-
-            return cost;
         }
     }
 }

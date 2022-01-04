2d8b3bb336211cb3072e1d585fb11872ac27aa2e
masafumi-yamamoto <mythmaker3633@yahoo.co.jp>
2022-01-04 14:22:44 +0900

    線形補間関数間違ってたので修正

--- BNSApp/Merge.cs
+++ BNSApp/Merge.cs
@@ -1,4 +1,6 @@
-﻿namespace BNSApp
+﻿using System;
+
+namespace BNSApp
 {
     public class Merge
     {
@@ -11,6 +13,7 @@
         /// <returns></returns>
         public static Pose LinearBlend(Pose forward, Pose backward, float ratio)
         {
+            ratio = MathF.Min(ratio, 1);
             var p0 = forward.Mult(1 - ratio);
             var p1 = backward.Mult(ratio);
             return p0.Add(p1);

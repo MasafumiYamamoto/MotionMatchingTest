ef69e056f11ab126d10faa28a12dca5e554ab408
masafumi-yamamoto <mythmaker3633@yahoo.co.jp>
2022-01-15 01:35:44 +0900

    NAを依然の値で埋めるだけの処理追加

---
+++ BNSApp/Solver/FillNull.cs
@@ -0,0 +1,22 @@
+﻿namespace BNSApp.Solver
+{
+    public class FillNull : ISolver
+    {
+        public string GetName()
+        {
+            return "FillNA";
+        }
+
+        public void Solve(MotionData motionData, MotionContainer trainData)
+        {
+            for (int loop = 0; loop < motionData.Loop; loop++)
+            {
+                var prevMeasuredId = loop * motionData.SkipFrames + 1;
+                for (int i = 1; i < motionData.SkipFrames; i++)
+                {
+                    motionData.PosList[prevMeasuredId - 1 + i] = motionData.PosList[prevMeasuredId - 1];
+                }
+            }
+        }
+    }
+}
